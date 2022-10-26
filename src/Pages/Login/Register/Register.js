540e5fbc583b347f97b68b54a04705a30c3235a5
Dipra Das <dipradas5940@gmail.com>
2022-10-26 19:49:47 +0600

    Register Setup

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,28 +3,28 @@ import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
 
 const Register = () => {
-    // return (
-    //     <div style={{ display: 'flex', justifyContent: 'center' }} className="mt-16">
-    //         <div style={{ marginLeft: '30px' }} className="card card-compact w-96 bg-base-100 shadow-xl  py-5">
-    //             <div className="card-body">
-    //                 <img style={{ width: '40px' }} src={logo} alt="Shoes" />
-    //                 <h1 style={{ fontSize: '35px', marginTop: '10px' }} className="card-title">Sign Up</h1>
-    //                 <p>Already Registered? <Link className='text-blue-600 underline font-semibold' to='/login'>Login</Link> </p>
-    //                 <label className='mt-3' htmlFor="">Full Name</label>
-    //                 <input type="text" placeholder="Enter your name" className="input input-bordered" />
-    //                 <label className='' htmlFor="">Photo URL</label>
-    //                 <input type="text" placeholder="Enter your Photo URL" className="input input-bordered" />
-    //                 <label className='' htmlFor="">Email</label>
-    //                 <input type="email" placeholder="Enter your email" className="input input-bordered" />
-    //                 <label className='' htmlFor="">Password</label>
-    //                 <input type="email" placeholder="Enter your password" className="input input-bordered " />
-    //                 <div className="card-actions">
-    //                     <button className="btn btn-primary btn-block mt-4">Register</button>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }} className="mt-16">
+            <div style={{ marginLeft: '30px' }} className="card card-compact w-96 bg-base-100 shadow-xl  py-5">
+                <div className="card-body">
+                    <img style={{ width: '40px' }} src={logo} alt="Shoes" />
+                    <h1 style={{ fontSize: '35px', marginTop: '10px' }} className="card-title">Sign Up</h1>
+                    <p>Already Registered? <Link className='text-blue-600 underline font-semibold' to='/login'>Login</Link> </p>
+                    <label className='mt-3' htmlFor="">Full Name</label>
+                    <input type="text" placeholder="Enter your name" className="input input-bordered" />
+                    <label className='' htmlFor="">Photo URL</label>
+                    <input type="text" placeholder="Enter your Photo URL" className="input input-bordered" />
+                    <label className='' htmlFor="">Email</label>
+                    <input type="email" placeholder="Enter your email" className="input input-bordered" />
+                    <label className='' htmlFor="">Password</label>
+                    <input type="email" placeholder="Enter your password" className="input input-bordered " />
+                    <div className="card-actions">
+                        <button className="btn btn-primary btn-block mt-4">Register</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Register;
