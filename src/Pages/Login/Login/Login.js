53cbdcf8a965d48eb4b0ab407878773088d5e3fc
Dipra Das <dipradas5940@gmail.com>
2022-10-27 00:12:09 +0600

    Email Pass Authntication Reg

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -5,6 +5,10 @@ import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
 
 const Login = () => {
+    const hlo = event => {
+        event.preventDefault();
+        console.log('grg');
+    }
     return (
         <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '60px' }} className="mt-16">
             <div style={{ marginLeft: '30px' }} className="card card-compact w-96 bg-base-100 shadow-xl  py-5">
@@ -12,16 +16,18 @@ const Login = () => {
                     <img style={{ width: '40px' }} src={logo} alt="Shoes" />
                     <h1 style={{ fontSize: '35px', marginTop: '10px' }} className="card-title">Sign In</h1>
                     <p>Do you have an account? <Link className='text-blue-600 underline font-semibold' to='/register'>Register</Link> </p>
-                    <label className='mt-3' htmlFor="">Email</label>
-                    <input type="email" placeholder="Enter your email" className="input input-bordered outline-none" />
-                    <label className='mt-2' htmlFor="">Password</label>
-                    <input type="email" placeholder="Enter your password" className="input input-bordered outline-none" />
-                    <div className="card-actions">
-                        <button className="btn btn-primary btn-block mt-5">Sign In</button>
-                    </div>
+                    <form>
+                        <label className='mt-3' htmlFor="">Email</label><br />
+                        <input type="email" placeholder="Enter your email" className="input input-bordered w-full" /><br />
+                        <label className='mt-2' htmlFor="">Password</label><br />
+                        <input type="email" placeholder="Enter your password" className="input input-bordered w-full" />
+                        <div className="card-actions">
+                            <button onSubmit={hlo} className="btn btn-primary btn-block mt-5">Sign In</button>
+                        </div>
+                    </form>
                     <div className="googleGithub flex justify-between">
-                        <div className='w-2/4 mr-2'><button className="btn btn-outline btn-block mt-1"><FaGoogle style={{fontSize: '20px'}}></FaGoogle></button></div>
-                        <div className='w-2/4 ml-2'><button className="btn btn-outline btn-block mt-1"> <FaGithub style={{fontSize: '24px'}}></FaGithub></button></div>
+                        <div className='w-2/4 mr-2'><button className="btn btn-outline btn-block mt-1"><FaGoogle style={{ fontSize: '20px' }}></FaGoogle></button></div>
+                        <div className='w-2/4 ml-2'><button className="btn btn-outline btn-block mt-1"> <FaGithub style={{ fontSize: '24px' }}></FaGithub></button></div>
                     </div>
                 </div>
             </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Register = () => {
+    const { createUser, updateUser } = useContext(AuthContext);
+    const [error, setError] = useState('');
+    const [clicked, setClicked] = useState(false);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('dsgsd')
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photoURL, email, password);
+        setError('');
+
+        createUser(email, password)
+            .then(result => {
+                handleUpdateUser(name, photoURL);
+                const user = result.user;
+                console.log(user);
+                form.reset();
+            })
+            .catch(error => {
+                console.error('error', error);
+                setError(error.message);
+            })
+    }
+
+    const handleUpdateUser = (name, photoURL) => {
+        const profile = {
+            displayName: name,
+            photoURL: photoURL
+        }
+        updateUser(profile);
+
+    }
+    const handleClicked = event => {
+        setClicked(event.target.checked);
+    }
     return (
         <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '60px' }} className="mt-16">
             <div style={{ marginLeft: '30px' }} className="card card-compact w-96 bg-base-100 shadow-xl  py-5">
@@ -10,17 +50,20 @@ const Register = () => {
                     <img style={{ width: '40px' }} src={logo} alt="Shoes" />
                     <h1 style={{ fontSize: '35px', marginTop: '10px' }} className="card-title">Sign Up</h1>
                     <p>Already Registered? <Link className='text-blue-600 underline font-semibold' to='/login'>Login</Link> </p>
-                    <label className='mt-3' htmlFor="">Full Name</label>
-                    <input type="text" placeholder="Enter your name" className="input input-bordered" />
-                    <label className='' htmlFor="">Photo URL</label>
-                    <input type="text" placeholder="Enter your Photo URL" className="input input-bordered" />
-                    <label className='' htmlFor="">Email</label>
-                    <input type="email" placeholder="Enter your email" className="input input-bordered" />
-                    <label className='' htmlFor="">Password</label>
-                    <input type="email" placeholder="Enter your password" className="input input-bordered " />
-                    <div className="card-actions">
-                        <button className="btn btn-primary btn-block mt-4">Register</button>
-                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <label className='mt-3' htmlFor="">Full Name</label><br />
+                        <input name='name' type="text" placeholder="Enter your name" className="input input-bordered w-full" /><br />
+                        <label className='' htmlFor="">Photo URL</label><br />
+                        <input name='photo' type="text" placeholder="Enter your Photo URL" className="input input-bordered w-full" /><br />
+                        <label className='mt-3' htmlFor="">Email</label><br />
+                        <input name='email' type="email" placeholder="Enter your email" className="input input-bordered w-full" /><br />
+                        <label className='mt-3' htmlFor="">Password</label><br />
+                        <input name='password' type="password" placeholder="Enter your password" className="input input-bordered w-full" /><br />
+                        <div className="card-actions">
+                            <button className="btn btn-primary btn-block mt-4">Register</button>
+                        </div>
+                    </form>
+                    <p className='label-text-alt text-red-600'>{error}</p>
                 </div>
             </div>
         </div>
