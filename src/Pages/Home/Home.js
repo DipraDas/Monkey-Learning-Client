45423b91e274ceca3b81253bef49e209cfefe27f
Dipra Das <dipradas5940@gmail.com>
2022-10-27 17:46:04 +0600

    Home Page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <div className="h-back">
+                <img style={{ width: '60px' }} src={logo} alt="" />
+                <h1>Monkey Learning</h1>
+                <p className='text-gray-200 text-xl tracking-wide'>- - - Love to learn something new - - -</p>
+                <Link to="/courses"><button className="btn btn-primary mt-7">Visit Our Courses</button></Link>
+            </div>
         </div>
     );
 };
